ed7d9f69b2dbd2ddd76c3028084d01134ce9f60e
uo270992 <uo270992@uniovi.es>
2022-08-30 20:25:49 +0200

    Implemented testing for the cost methods

--- Kata_Nextail/src/test/tests.java
+++ Kata_Nextail/src/test/tests.java
@@ -7,46 +7,47 @@ import java.util.Hashtable;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import checkout.Checkout;
+import checkout.Pants;
+import checkout.Product;
+import checkout.Tshirt;
+import checkout.Voucher;
 
 class tests {
 	Hashtable<String, Double> pricing_rules = new Hashtable<String, Double>();
-	Checkout my_checkout = new Checkout (pricing_rules);;
+	Checkout my_checkout = new Checkout(pricing_rules);
 	
+	//This @Before does not work, probably due to the JUnit version.
 	@Before
-	void setUp() {		 	
+	void onSetUp() {		 	
 		pricing_rules.put("VOUCHER", 5.00);
 		pricing_rules.put("TSHIRT", 20.00);
-		pricing_rules.put("PANTS", 7.50);	
+		pricing_rules.put("PANTS", 7.50);
+		System.out.print(pricing_rules.get("VOUCHER"));
+		my_checkout = new Checkout (pricing_rules);
 	}
 	
 	@Test
 	void test_voucher() {
-		assertEquals(5, my_checkout.total_voucher(2, pricing_rules.get("PANTS")));
+		pricing_rules.put("VOUCHER", 5.00);
+		assertEquals(5.00, my_checkout.total_voucher(2, pricing_rules.get("VOUCHER")));
 	}
 	
 	@Test
 	void test_pants() {
-		assertEquals(15, my_checkout.total_voucher(2, pricing_rules.get("PANTS")));
+		pricing_rules.put("PANTS", 7.50);
+		assertEquals(15.00, my_checkout.total_pants(2, pricing_rules.get("PANTS")));
 	}
 	
 	@Test
 	void test_tshirt() {
-		assertEquals(57, my_checkout.total_voucher(3, pricing_rules.get("TSHIRT")));
+		pricing_rules.put("TSHIRT", 20.00);
+		assertEquals(57.00, my_checkout.total_tshirt(3, pricing_rules.get("TSHIRT")));
 	}
-	@Test
-	void test_total() {
-		List<String> my_products = new ArrayList<String>();
-		my_products.add("VOUCHER");
-		my_products.add("TSHIRT");
-		my_products.add("VOUCHER");
-		my_products.add("VOUCHER");
-		my_products.add("PANTS");
-		my_products.add("TSHIRT");
-		my_products.add("TSHIRT");
-		assertEquals(74.50, my_checkout.total_value(my_products));
-	}
+	
+	
 
 }
